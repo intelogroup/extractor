c51dc077cc1cb98b1b08f576556113944f62bec3
Jay_code101 <64654010+intelogroup@users.noreply.github.com>
2025-02-10 11:29:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Create virtual environment
-python -m venv venv
-
-# Activate virtual environment
-source venv/bin/activate
-
-# Install dependencies
-pip install streamlit==1.32.0
-pip install PyPDF2==3.0.1
-pip install gspread==6.0.2
-pip install google-auth==2.28.1
-pip install google-auth-oauthlib==1.2.0
-pip install pyyaml==6.0.1
+pip install --upgrade pip
+pip install -r requirements.txt
